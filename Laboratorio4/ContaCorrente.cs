3ad1cf477c0b41179429fc4d332110b2d53d96c2
Camila Rezende <csalinor@emeal.nttdata.com>
2022-01-06 10:26:34 -0300

    Exerciciolab04

---
+++ Laboratorio4/ContaCorrente.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio4
+{
+    internal class ContaCorrente
+    { 
+        
+        private decimal saldo;
+        public void Depositar(decimal val)
+        {
+            saldo = saldo + val;
+            contador++; //contator serve para contar quantas vezes foram feitas as transações
+            acumulador = acumulador + saldo; //soma todos os saldos após as transções
+            saldomediaContaT = acumulador / contador; //media do saldo final após as transações
+
+        }
+        public void Sacar(decimal val)
+        {
+            saldo = saldo - val;
+            contador++; //contator serve para contar quantas vezes foram feitas as transações
+            acumulador = acumulador + saldo; //soma todos os saldos após as transções
+            saldomediaContaT = acumulador / contador; //media do saldo final após as transações
+        }
+        public decimal Saldo
+        {
+            get { return saldo; }
+        }
+        public ContaCorrente(decimal val)
+        {
+            saldo = val;
+        }
+        
+        public DateTime dataAberturaConta { get; set; } //PROPRIEDADES DE ACESSO get = pegar o valor da vaariavel
+                                                        //set = grava o valor da variável 
+        public string nomeTitular { get; set; }
+
+        public decimal saldomediaContaT { get; set; }
+        public int contador { get; set; }
+        public decimal acumulador { get; set; }
+
+        public ContaCorrente(string v1)
+        {
+            nomeTitular = v1;
+            dataAberturaConta = DateTime.Now;
+            contador = 0; //contator serve para contar quantas vezes foram feitas as transações
+            acumulador = 0;
+        }
+
+    }
+}
---
+++ Laboratorio4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Laboratorio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("-----exercício 2 -----");
+            ContaCorrente minhaConta = new ContaCorrente(340);
+            Console.WriteLine("Saldo da conta R$ " + minhaConta.Saldo); 
+
+            minhaConta.Depositar(100); 
+            Console.WriteLine("Valor do deposito R$ " + minhaConta.Saldo);
+            
+            minhaConta.Sacar(50);
+            Console.WriteLine("Valor do saque R$ " + minhaConta.Saldo);
+            Console.ReadKey();
+
+            Console.WriteLine("");
+            Console.WriteLine("-----exercício 4.1 -----");
+            ContaCorrente conta = new ContaCorrente("Camila Rezende");
+            conta.Depositar(100);
+            conta.Depositar(100);
+            conta.Depositar(100);
+            conta.Sacar(200);
+            Console.WriteLine("Valor média da conta R$ " + conta.saldomediaContaT);
+            Console.ReadKey();
+
+        }
+    }
+}
